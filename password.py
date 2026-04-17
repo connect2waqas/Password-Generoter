b80ce1f7a9a9d337e0d77a58ce1fc678ad6b0542
waqas khan <207045044+connect2waqas@users.noreply.github.com>
2026-04-17 20:34:32 +0500

    Uncomment and refine password validation function

--- password.py
+++ password.py
@@ -1,21 +1,21 @@
-# def Password():
-#     password = input("Enter Your password: ")
-#     if len(password) >= 8:
-#         if password.isalnum():
-#                 print("Your password is set.")
-#         else:
-#             print("Enter a combinition of digit and alphabet")
-#     else:
-#         print("Password length should be greator or equal to eight.")
-#     return password
-# print(Password())
+def Password():
+    password = input("Enter Your password: ")
+    if len(password) >= 8:
+        if password.isalnum():
+                print("Your password is set.")
+        else:
+            print("Enter a combinition of digit and alphabet")
+    else:
+        print("Password length should be greator or equal to eight.")
+    return password
+print(Password())
 
 
-# def Password():
-#     password = input("Enter you Password: ")
-#     if len(password) >=8 and (password.isalnum()):
-#         print("Your password is set")
-#     else:
-#         print("Your password length should be eight or greater and alphnumeric.")
-#     return f"Password is :", password
-# print(Password())
+def Password():
+    password = input("Enter you Password: ")
+    if len(password) >=8 and (password.isalnum()):
+        print("Your password is set")
+    else:
+        print("Your password length should be eight or greater and alphnumeric.")
+    return f"Password is :", password
+print(Password())
